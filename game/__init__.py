fbd4d5cdbb1121c4cf52c36268e4743d52999f5f
312-19\lky9450 <lky9450@gmail.com>
2024-12-26 10:06:56 +0900

    파이썬패키지연습(클래스 모듈__init__)

---
+++ game/__init__.py
@@ -0,0 +1,13 @@
+from . graphic.render import render_test
+
+VERSION = 3.11
+
+def print_version_info():
+    print(f'The version of this game is {VERSION}')
+    print("게임 제작자 : 이규현")
+    print("게임 제작자 이메일 :lky9450@gmail.com")
+    print("게임 제작의뢰 : 010-1231-1231")
+
+# 패키지 실행 초기화 문구
+print("Initializing game...")
+print("Loading...")
